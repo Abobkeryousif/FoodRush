3f2402c24726c5097e7675d8553db3e8bcbc4f3c
bakry <bakryhassona7@gmail.com>
2025-09-15 17:39:43 +0300

    Start With Reviews

---
+++ FoodRush.Domain/Entites/Review.cs
@@ -0,0 +1,19 @@
+﻿
+namespace FoodRush.Domain.Entites
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int restaurantId { get; set; }
+        public int userId { get; set; }
+        public string? Comment { get; set; }
+
+        [Range(1,5)]
+        public double Rating { get; set; }
+        public DateTime createOn { get; set; } = DateTime.Now;
+
+        public Restaurant Restaurant { get; set; }  
+        public User User { get; set; }
+
+    }
+}
